0471e48f6ed5a51f7ce53cdad9af0bdd5b0db0c6
DESKTOP-NH7VU9B\chris <christianharris0403@gmail.com>
2025-06-06 19:31:23 +0100

    added testing file

---
+++ testing/test1.py
@@ -0,0 +1,23 @@
+import pygame
+import src.PyUI as pyui
+
+pygame.init()
+screenw = 1200
+screenh = 900
+screen = pygame.display.set_mode((screenw, screenh), pygame.RESIZABLE)
+ui = pyui.UI()
+done = False
+clock = pygame.time.Clock()
+
+ui.makebutton(10,10,'test')
+
+while not done:
+    for event in ui.loadtickdata():
+        if event.type == pygame.QUIT:
+            done = True
+    screen.fill(pyui.Style.wallpapercol)
+
+    ui.rendergui(screen)
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()
