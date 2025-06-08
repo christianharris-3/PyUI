658f06209ea51d72b89c796e8d1963aace6a55f3
Christopher Brocklebank <68190524+CBrocks294@users.noreply.github.com>
2025-06-08 19:22:53 +0100

    Unpacking typed dict paramters (#6)
    * Made file for PositionalData dictionary for passing variables
    
    * Started making DataClasses
    
    * Slight modification to dataclasses and guiitem error throwing
    
    * Added object params files

--- UIpygame/ObjectParameters/ButtonObjParams().py
+++ UIpygame/ObjectParameters/ButtonObjParams().py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass
+
 from UIpygame.DataClasses.BorderData import BorderData
 from UIpygame.DataClasses.ClickableData import ClickableData
 from UIpygame.DataClasses.ObjectData import ObjectData
 from UIpygame.DataClasses.PositionalData import PositionalData
 from UIpygame.DataClasses.StyleData import StyleData
 from UIpygame.DataClasses.TextData import TextData
-import pygame
 
 
 @dataclass
-class AllObjParams(BorderData, ClickableData, ObjectData, PositionalData, StyleData, TextData):
-    wallpaper_col: pygame.Color | tuple[int] = (255, 255, 255)
+class ButtonOjbParams(BorderData, ClickableData, ObjectData, PositionalData, StyleData, TextData):
+    pass
--- UIpygame/ObjectParameters/RectangleObjParams.py
+++ UIpygame/ObjectParameters/RectangleObjParams.py
@@ -1,13 +1,12 @@
 from dataclasses import dataclass
-from UIpygame.DataClasses.BorderData import BorderData
+
 from UIpygame.DataClasses.ClickableData import ClickableData
 from UIpygame.DataClasses.ObjectData import ObjectData
 from UIpygame.DataClasses.PositionalData import PositionalData
 from UIpygame.DataClasses.StyleData import StyleData
-from UIpygame.DataClasses.TextData import TextData
-import pygame
+
 
 
 @dataclass
-class AllObjParams(BorderData, ClickableData, ObjectData, PositionalData, StyleData, TextData):
-    wallpaper_col: pygame.Color | tuple[int] = (255, 255, 255)
+class RectangleObjParams(ClickableData,ObjectData, PositionalData, StyleData):
+    pass
